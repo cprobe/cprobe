58e2896c0621963260ce89a36565f01c354ee3b6
Ulric Qin <ulric.qin@gmail.com>
2023-12-02 22:42:51 +0800

    fix typo

--- plugins/interface.go
+++ plugins/interface.go
@@ -9,7 +9,7 @@ import (
 type Plugin interface {
 	// ParseConfig is used to parse config
 	ParseConfig(bs []byte) (any, error)
-	// Scrape is used to scrape metrics, cfg need to be cost specific cfg
+	// Scrape is used to scrape metrics, cfg need to be cast specific cfg
 	Scrape(ctx context.Context, target string, cfg any, ss *types.Samples) error
 }
 
