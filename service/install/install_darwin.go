3c3beecb4e692751658ff7665f923e38ba4eca45
Ulric Qin <ulric.qin@gmail.com>
2023-12-04 14:51:38 +0800

    fix darwin install

--- service/install/install_darwin.go
+++ service/install/install_darwin.go
@@ -1,6 +1,11 @@
 package install
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/cprobe/cprobe/flags"
 	"github.com/kardianos/service"
 )
 
@@ -20,5 +25,24 @@ var (
 )
 
 func ServiceConfig() (*service.Config, error) {
+	ov, err := os.Executable()
+	if err == nil {
+		if len(filepath.Dir(ov)) != 0 {
+			serviceConfig.WorkingDirectory = filepath.Dir(ov)
+		}
+	} else {
+		return nil, fmt.Errorf("failed to get executable path: %s", err)
+	}
+
+	if flags.ConfigDirectory == "" {
+		return nil, fmt.Errorf("-conf.d is blank")
+	}
+
+	absPath, err := filepath.Abs(flags.ConfigDirectory)
+	if err != nil {
+		return nil, err
+	}
+
+	serviceConfig.Arguments = append(serviceConfig.Arguments, "-conf.d", absPath)
 	return serviceConfig, nil
 }
